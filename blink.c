896eb1dfade672826ac38d385e10d3b11872f276
Nick Hebdon <nick@rigare.co.uk>
2022-04-18 14:27:03 +0100

    selectable between old code and new

--- blink.c
+++ blink.c
@@ -5,11 +5,15 @@
  *
  * */
 #include "sam.h"
+#define IMPROPER
+#ifdef IMPROPER
 #define GPIO_BASE 0x41004400
 #define PORTDIRSET 0x41004408
 #define PORTOUT 0x41004410
 #define PORTCLR 0x41004414
 #define LED_GPIO_BIT 10
+#endif
+
 
 /** GPIO Register set */
 volatile unsigned int* gpio;
@@ -27,13 +31,15 @@ uint32_t pin = 10;
 uint32_t pinMask = (1ul << pin);
 
 	/* Set output direction for LED pin*/
-//	gpio = (unsigned int*)PORTDIRSET;
-//	*gpio |= (1 << LED_GPIO_BIT);
+#ifdef IMPROPER
+	gpio = (unsigned int*)PORTDIRSET;
+	*gpio |= (1 << LED_GPIO_BIT);
+#else
       // enable input, to support reading back values, with pullups disabled
       PORT->Group[port].PINCFG[pin].reg = (uint8_t) (PORT_PINCFG_INEN | PORT_PINCFG_DRVSTR);
       // Set pin to output mode
       PORT->Group[port].DIRSET.reg = pinMask;
-
+#endif
 
 
 	/* Never exit */
@@ -42,24 +48,29 @@ uint32_t pinMask = (1ul << pin);
 
   if (count <= 1) //10ms * 10000 = 100secs ish, check this works before adding to microbian port for first test recovery
   {
-     force_bootloader();  
+     force_bootloader();
   }
 
 		for (tim = 0; tim < 500000; tim++)
 			;
 
 		/* Clear LED output pin*/
-//		gpio = (unsigned int*)PORTCLR;
-//		*gpio = (1 << LED_GPIO_BIT);
+#ifdef IMPROPER
+		gpio = (unsigned int*)PORTCLR;
+		*gpio = (1 << LED_GPIO_BIT);
+#else
                 PORT->Group[port].OUTCLR.reg = pinMask;
-
+#endif
 
 		for (tim = 0; tim < 500000; tim++)
 			;
 
 		/* Set LED output pin*/
-		//gpio = (unsigned int*)PORTOUT;
-		//*gpio = (1 << LED_GPIO_BIT);
+#ifdef IMPROPER
+		gpio = (unsigned int*)PORTOUT;
+		*gpio = (1 << LED_GPIO_BIT);
+#else
                 PORT->Group[port].OUTSET.reg = pinMask;
+#endif
 	}
 }
